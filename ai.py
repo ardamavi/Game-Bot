30a0fffd682d01dda03e04150d2b4088cf1f3d53
Elijah V. Schrotenboer <fidgetybog@gmail.com>
2021-01-24 20:24:51 -0500

    Update ai.py

--- ai.py
+++ ai.py
@@ -1,3 +1,4 @@
+
 # Arda Mavi
 import os
 import platform
@@ -9,6 +10,8 @@ from predict import predict
 from game_control import *
 from keras.models import model_from_json
 
+import pickle
+
 def main():
     # Get Model:
     model_file = open('Data/Model/model.json', 'r')
@@ -18,7 +21,9 @@ def main():
     model.load_weights("Data/Model/weights.h5")
 
     print('AI start now!')
-
+    with open('listfile.data', 'rb') as filehandle:
+        # read the data as binary data stream
+        placesList = pickle.load(filehandle)
     while 1:
         # Get screenshot:
         screen = ImageGrab.grab()
@@ -26,12 +31,16 @@ def main():
         screen = np.array(screen)
         # 4 channel(PNG) to 3 channel(JPG)
         Y = predict(model, screen)
+        print(Y)
+        Y = placesList[Y]
+        print(Y)
+        
         if Y == [0,0,0,0]:
             # Not action
             continue
         elif Y[0] == -1 and Y[1] == -1:
             # Only keyboard action.
-            key = get_key(Y[3])
+            key = get_key(int(Y[3]))
             if Y[2] == 1:
                 # Press:
                 press(key)
@@ -40,19 +49,19 @@ def main():
                 release(key)
         elif Y[2] == 0 and Y[3] == 0:
             # Only mouse action.
-            click(Y[0], Y[1])
+            click(int(Y[0]), int(Y[1]))
         else:
             # Mouse and keyboard action.
             # Mouse:
-            click(Y[0], Y[1])
+            click(int(Y[0]), int(Y[1]))
             # Keyboard:
-            key = get_key(Y[3])
+            key = get_key(int(Y[3]))
             if Y[2] == 1:
                 # Press:
                 press(key)
             else:
                 # Release:
                 release(key)
-
+        
 if __name__ == '__main__':
     main()
