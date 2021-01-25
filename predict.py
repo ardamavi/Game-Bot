7523ff6f9b3cbf2b476f987f80c84d711c70dd4c
Elijah V. Schrotenboer <fidgetybog@gmail.com>
2021-01-24 20:24:35 -0500

    Update predict.py

--- predict.py
+++ predict.py
@@ -3,6 +3,7 @@ import numpy as np
 from scipy.misc import imresize
 
 def predict(model, X):
-    X = imresize(X, (150, 150, 3)).astype('float32')/255.
+    X = imresize(X, (150, 150, 3)).astype('float32')
     Y = model.predict(X.reshape(1,150,150,3))
+    Y = Y.argmax()
     return Y
