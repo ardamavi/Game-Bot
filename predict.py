946e90eb605f97255c1303aee7140ce2cc50b3a3
Elijah V. Schrotenboer <fidgetybog@gmail.com>
2021-01-31 14:47:38 -0500

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,9 +1,9 @@
 # Arda Mavi
 import numpy as np
-from scipy.misc import imresize
+import imageio
 
 def predict(model, X):
-    X = imresize(X, (150, 150, 3)).astype('float32')
+    X = imageio.imresize(X, (150, 150, 3)).astype('float32') /255.
     Y = model.predict(X.reshape(1,150,150,3))
     Y = Y.argmax()
     return Y
