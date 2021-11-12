c472ca69f725bc2ccb53fac71c72ae1aa2318685
Elijah V. Schrotenboer <fidgetymodder@gmail.com>
2021-11-12 12:47:27 -0500

    Update ai.py

--- ai.py
+++ ai.py
@@ -4,12 +4,12 @@ import os
 import platform
 import numpy as np
 from time import sleep
-from PIL import ImageGrab
+from PIL import Image
 from game_control import *
 from predict import predict
 from game_control import *
 from keras.models import model_from_json
-
+from mss import mss
 import pickle
 
 def main():
@@ -26,21 +26,24 @@ def main():
         placesList = pickle.load(filehandle)
     while 1:
         # Get screenshot:
-        screen = ImageGrab.grab()
-        # Image to numpy array:
-        screen = np.array(screen)
+        with mss() as sct:
+            monitor = sct.monitors[1]
+            sct_img = sct.grab(monitor)
+            # Convert to PIL/Pillow Image
+            screen = Image.frombytes('RGB', sct_img.size, sct_img.bgra, 'raw', 'BGRX')
+            screen = np.array(screen)[:,:,:3] # Get first 3 channel from image as numpy array.
         # 4 channel(PNG) to 3 channel(JPG)
         Y = predict(model, screen)
         print(Y)
         Y = placesList[Y]
+        Y = [int(i) for i in Y]
         print(Y)
-        
         if Y == [0,0,0,0]:
             # Not action
             continue
         elif Y[0] == -1 and Y[1] == -1:
             # Only keyboard action.
-            key = get_key(int(Y[3]))
+            key = get_key(Y[3])
             if Y[2] == 1:
                 # Press:
                 press(key)
@@ -48,8 +51,9 @@ def main():
                 # Release:
                 release(key)
         elif Y[2] == 0 and Y[3] == 0:
-            # Only mouse action.
-            click(int(Y[0]), int(Y[1]))
+            # Click action.
+            click(Y[0], Y[1])
+        '''
         else:
             # Mouse and keyboard action.
             # Mouse:
@@ -62,6 +66,7 @@ def main():
             else:
                 # Release:
                 release(key)
-        
+        '''
+        time.sleep(0.005)
 if __name__ == '__main__':
     main()
